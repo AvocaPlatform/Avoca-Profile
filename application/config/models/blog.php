1211391a36a6e30180845ca59ee07fbda7780c2a
Jacky <tdhungit@gmail.com>
2018-12-18 17:36:36 +0700

    modules management

---
+++ application/config/models/blog.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by AVOCA.IO
+ * Website: http://avoca.io
+ * User: Jacky
+ * Email: hungtran@up5.vn | jacky@youaddon.com
+ * Person: tdhungit@gmail.com
+ * Skype: tdhungit
+ * Git: https://github.com/tdhungit
+ */
+
+return [
+    'title' => 'name',
+    'list' => [
+        'name' => [],
+        'image' => [],
+        'date_publish' => [],
+        'category' => [],
+        'is_hot' => [],
+    ],
+    'record' => [
+        'image' => [],
+        'name' => [],
+        'slug' => [],
+        'date_publish' => [],
+        'category' => [],
+        'is_hot' => ['type' => 'date'],
+        'summary' => ['type' => 'date'],
+        'content' => ['type' => 'textarea'],
+    ]
+];
---
+++ application/config/models/page.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by AVOCA.IO
+ * Website: http://avoca.io
+ * User: Jacky
+ * Email: hungtran@up5.vn | jacky@youaddon.com
+ * Person: tdhungit@gmail.com
+ * Skype: tdhungit
+ * Git: https://github.com/tdhungit
+ */
+
+return [
+    'title' => 'name',
+    'list' => [
+        'name' => [],
+        'slug' => [],
+        'meta_keyword' => [],
+    ],
+    'record' => [
+        'name' => [],
+        'slug' => [],
+        'content' => [],
+        'meta_keyword' => [],
+        'meta_description' => [],
+    ]
+];
